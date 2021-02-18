5504b1c5de460d4e5103df342ce0d52069e7c2f0
Juan Cruz <jc23729@gmail.com>
2021-02-18 03:34:05 +0000

    update

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 #Fix the code below 👇
 
 print("Day 1 - String Manipulation")
-print("String Concatenation is done with the "+" sign.")
+#2. Outer double quotes changed to single quotes.
+print('String Concatenation is done with the "+" sign.')
   print('e.g. print("Hello " + "world")')
 print(("New lines can be created with a backslash and n.")
 
